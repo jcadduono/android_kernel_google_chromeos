6bc1e82e636642f2ade5fc41a4f62f939d0e0b29
Derek Basehore <dbasehore@chromium.org>
2014-09-09 20:50:10 -0700

    FIXUP: FROMLIST: clk: Add safe switch hook
    This fixes a bug where POST_RATE_CHANGE events were never sent. After changing
    the rate for a clock. This bug was introduced in "FROMLIST: clk: Add safe switch
    hook"
    
    BUG=None
    TEST=build and boot on pinky with cpufreq patches
    
    Change-Id: I421a350e953659236a4cff20b8c1a7fd1ebc2185
    Signed-off-by: Derek Basehore <dbasehore@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/217422
    Reviewed-by: Stephen Boyd <sboyd@codeaurora.org>
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    Tested-by: Doug Anderson <dianders@chromium.org>
    Commit-Queue: Doug Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/343961
    Commit-Ready: Sreedhar Sambangi <ssambang@codeaurora.org>
    Tested-by: Sreedhar Sambangi <ssambang@codeaurora.org>
    Reviewed-by: Grant Grundler <grundler@chromium.org>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -1716,6 +1716,7 @@ static struct clk *clk_propagate_rate_change(struct clk *clk, unsigned long even
 	case PRE_RATE_CHANGE:
 		if (clk->safe_parent)
 			clk->ops->set_parent(clk->hw, clk->safe_parent_index);
+		clk->old_rate = clk->rate;
 		break;
 	case POST_RATE_CHANGE:
 		if (clk->safe_parent) {
@@ -1738,8 +1739,8 @@ static struct clk *clk_propagate_rate_change(struct clk *clk, unsigned long even
 	}
 
 	if (clk->notifier_count) {
-		if (event != POST_RATE_CHANGE)
-			ret = __clk_notify(clk, event, clk->rate,
+		if (event != POST_RATE_CHANGE || clk->old_rate != clk->rate)
+			ret = __clk_notify(clk, event, clk->old_rate,
 					   clk->new_rate);
 		if (ret & NOTIFY_STOP_MASK && event != POST_RATE_CHANGE)
 			fail_clk = clk;
