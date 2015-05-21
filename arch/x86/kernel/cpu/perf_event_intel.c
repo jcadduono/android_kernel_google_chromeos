51cd796f80de69a2fda39d8fd69f6c4ea0a4f9c3
Peter Zijlstra <peterz@infradead.org>
2015-05-21 12:38:21 +0200

    UPSTREAM: perf/x86/intel: Add lockdep assert
    Lockdep is very good at finding incorrect IRQ state while locking and
    is far better at telling us if we hold a lock than the _is_locked()
    API. It also generates less code for !DEBUG kernels.
    
    BUG=b:33115527
    TEST=Verified that it compiles
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vince Weaver <vincent.weaver@maine.edu>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit b32ed7f5de262b10633bb6c6dbb7f8ba46598cf4)
    Signed-off-by: Gabriel Marin <gmx@chromium.org>
    
    Change-Id: Iba6ea821bf9e233d0e8e9c0580d87b4fa3b80af8
    Reviewed-on: https://chromium-review.googlesource.com/417214
    Commit-Ready: Gabriel Marin <gmx@chromium.org>
    Tested-by: Gabriel Marin <gmx@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>

--- arch/x86/kernel/cpu/perf_event_intel.c
+++ arch/x86/kernel/cpu/perf_event_intel.c
@@ -1905,7 +1905,6 @@ intel_start_scheduling(struct cpu_hw_events *cpuc)
 	 * in stop_event_scheduling()
 	 * makes scheduling appear as a transaction
 	 */
-	WARN_ON_ONCE(!irqs_disabled());
 	raw_spin_lock(&excl_cntrs->lock);
 
 	/*
@@ -2189,7 +2188,7 @@ static void intel_commit_scheduling(struct cpu_hw_events *cpuc,
 
 	xl = &excl_cntrs->states[tid];
 
-	WARN_ON_ONCE(!raw_spin_is_locked(&excl_cntrs->lock));
+	lockdep_assert_held(&excl_cntrs->lock);
 
 	if (cntr >= 0) {
 		if (c->flags & PERF_X86_EVENT_EXCL)
