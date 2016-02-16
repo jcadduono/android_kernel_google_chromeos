b0b0980a5201258224c3d1743743debb57c9ad67
Srinivasa Duvvuri <sduvvuri@google.com>
2016-02-16 10:30:49 -0800

    CHROMIUM: mac80211: enable rx stats histogram
    BUG=chrome-os-partner:45726
    TEST=Ran station connect,disconnect,iperf,dump rx/tx stats operations
         in a loop.
    
    Reviewed-on: https://chromium-review.googlesource.com/327682
    Commit-Ready: Srinivasa duvvuri <sduvvuri@chromium.org>
    Tested-by: Srinivasa duvvuri <sduvvuri@chromium.org>
    Reviewed-by: Julan Hsu <julanhsu@google.com>
    
    Change-Id: I4b0516b0295dcffa2b7eb7ae666c7d6d5de5b740
    Signed-off-by: Srinivasa duvvuri <sduvvuri@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/340409
    Commit-Ready: Yixiang Li <yixiang@google.com>
    Tested-by: Yixiang Li <yixiang@google.com>
    Reviewed-by: Grant Grundler <grundler@chromium.org>

--- net/mac80211-4.2/debugfs.c
+++ net/mac80211-4.2/debugfs.c
@@ -251,6 +251,8 @@ void debugfs_hw_add(struct ieee80211_local *local)
 	DEBUGFS_ADD(hwflags);
 	DEBUGFS_ADD(user_power);
 	DEBUGFS_ADD(power);
+	/* enable rx stats by default */
+	local->rx_stats_enabled=1;
 	debugfs_create_bool("rx_stats_enabled", S_IWUSR | S_IRUSR, phyd,
 			    &local->rx_stats_enabled);
 
