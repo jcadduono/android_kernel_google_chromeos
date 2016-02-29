f12478309d2d5587dfc02a325a736de6958c417e
Imre Deak <imre.deak@intel.com>
2016-02-29 22:10:33 +0200

    UPSTREAM: drm/i915: Add missing NULL check before calling initial_watermarks
    Not all platforms set this callback, so NULL check it before calling it.
    
    v2:
    - Call intel_update_watermarks() on HSW+ where the callback is not set.
      (Matt)
    
    CC: Matt Roper <matthew.d.roper@intel.com>
    Fixes: commit ed4a6a7ca853 ("drm/i915: Add two-stage ILK-style watermark programming (v11)")
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1456776633-3401-1-git-send-email-imre.deak@intel.com
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>
    
    BUG=chrome-os-partner:45364,chrome-os-partner:52008
    TEST=Hot plugging 4K displays via HDMI/DP to Skl systems should not cause
    TEST=flicker.
    
    (cherry picked from commit 1d5bf5d9d9ef0c1e639d36178a224d83888c5a29)
    Signed-off-by: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    
    Change-Id: I7f4a14981579dfbffe851ce5d0d9a45b92629db8
    Reviewed-on: https://chromium-review.googlesource.com/360024
    Commit-Ready: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    Tested-by: Prathyushi Nangia <prathyushi.nangia@intel.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -4772,7 +4772,8 @@ static void ironlake_crtc_enable(struct drm_crtc *crtc)
 	 */
 	intel_color_load_luts(crtc);
 
-	dev_priv->display.initial_watermarks(intel_crtc->config);
+	if (dev_priv->display.initial_watermarks != NULL)
+		dev_priv->display.initial_watermarks(intel_crtc->config);
 	intel_enable_pipe(intel_crtc);
 
 	if (intel_crtc->config->has_pch_encoder)
@@ -4858,7 +4859,10 @@ static void haswell_crtc_enable(struct drm_crtc *crtc)
 	intel_ddi_set_pipe_settings(crtc);
 	intel_ddi_enable_transcoder_func(crtc);
 
-	dev_priv->display.initial_watermarks(pipe_config);
+	if (dev_priv->display.initial_watermarks != NULL)
+		dev_priv->display.initial_watermarks(pipe_config);
+	else
+		intel_update_watermarks(crtc);
 	intel_enable_pipe(intel_crtc);
 
 	if (intel_crtc->config->has_pch_encoder)
