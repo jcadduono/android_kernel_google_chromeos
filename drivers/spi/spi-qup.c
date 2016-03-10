3da8ce8c34cfb127860481f74a624a63b2d6af60
Matthew McClintock <mmcclint@codeaurora.org>
2016-03-10 16:44:55 -0600

    CHROMIUM: spi: qup: properly detect extra interrupts
    It's possible for a SPI transaction to complete and get another
    interrupt and have it processed on the same spi_transfer before the
    transfer_one can set it to NULL.
    
    This masks unexpected interrupts, so let's set the spi_transfer to
    NULL in the interrupt once the transaction is done. So we can
    properly detect these bad interrupts and print warning messages.
    
    BUG=chrome-os-partner:49255
    TEST=SPI nor works in block mode
    
    Change-Id: I88444309c5a344ba2ddfaa3ac07e46c8ab1392b2
    Signed-off-by: Matthew McClintock <mmcclint@codeaurora.org>
    Reviewed-on: https://chromium-review.googlesource.com/335628
    Commit-Ready: Matthew McClintock <mmcclint@qca.qualcomm.com>
    Tested-by: Matthew McClintock <mmcclint@qca.qualcomm.com>
    Reviewed-by: Grant Grundler <grundler@chromium.org>

--- drivers/spi/spi-qup.c
+++ drivers/spi/spi-qup.c
@@ -507,6 +507,7 @@ static irqreturn_t spi_qup_qup_irq(int irq, void *dev_id)
 	u32 opflags, qup_err, spi_err;
 	unsigned long flags;
 	int error = 0;
+	bool done = 0;
 
 	spin_lock_irqsave(&controller->lock, flags);
 	xfer = controller->xfer;
@@ -565,16 +566,19 @@ static irqreturn_t spi_qup_qup_irq(int irq, void *dev_id)
 			spi_qup_write(controller, xfer);
 	}
 
-	spin_lock_irqsave(&controller->lock, flags);
-	controller->error = error;
-	controller->xfer = xfer;
-	spin_unlock_irqrestore(&controller->lock, flags);
-
 	/* re-read opflags as flags may have changed due to actions above */
 	opflags = readl_relaxed(controller->base + QUP_OPERATIONAL);
 
 	if ((controller->rx_bytes == xfer->len &&
 		(opflags & QUP_OP_MAX_INPUT_DONE_FLAG)) ||  error)
+		done = true;
+
+	spin_lock_irqsave(&controller->lock, flags);
+	controller->error = error;
+	controller->xfer = done ? NULL : xfer;
+	spin_unlock_irqrestore(&controller->lock, flags);
+
+	if (done)
 		complete(&controller->done);
 
 	return IRQ_HANDLED;
@@ -767,7 +771,6 @@ static int spi_qup_transfer_one(struct spi_master *master,
 exit:
 	spi_qup_set_state(controller, QUP_STATE_RESET);
 	spin_lock_irqsave(&controller->lock, flags);
-	controller->xfer = NULL;
 	if (!ret)
 		ret = controller->error;
 	spin_unlock_irqrestore(&controller->lock, flags);
