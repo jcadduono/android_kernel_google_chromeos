jcadduono/android_kernel_google_chromeos

commit fad686618b89f61fe9d37d676ff19bebc955ef29

CHROMIUM: ath10k: Configure platform type for QCA40XX
This patch configure platform type for QCA40XX

BUG=chrome-os-partner:49260
TEST=built ath10k modules for x86

Change-Id: Ibc278b857f6ed13d37c69c9c024db8c8ee8ab2a7
Signed-off-by: Maharaja Kennadyrajan <c_mkenna@qti.qualcomm.com>
Signed-off-by: Maharaja Kennadyrajan <mkenna@codeaurora.org>
Reviewed-on: https://chromium-review.googlesource.com/334116
Commit-Ready: Srinivasa duvvuri <sduvvuri@chromium.org>
Tested-by: Srinivasa duvvuri <sduvvuri@chromium.org>
Reviewed-by: Srinivasa duvvuri <sduvvuri@chromium.org>

drivers/net/wireless-4.2/ath/ath10k/core.c
@@ -164,6 +164,7 @@ static const struct ath10k_hw_params ath10k_hw_params_list[] = {
 		.rx_chain_mask = 0xf,
 		.max_spatial_stream = 4,
 		.cal_data_len = 12064,
+		.platform_type = WMI_HOST_PLATFORM_LOW_PERF,
 		.fw = {
 			.dir = QCA99X0_HW_2_0_FW_DIR,
 			.fw = QCA99X0_HW_2_0_FW_FILE,
@@ -1795,7 +1796,7 @@ int ath10k_core_start(struct ath10k *ar, enum ath10k_firmware_mode mode)
 			val = WMI_10_4_PEER_STATS;
 
 		status = ath10k_wmi_ext_resource_config(ar,
-							WMI_HOST_PLATFORM_HIGH_PERF, val);
+							ar->hw_params.platform_type, val);
 		if (status) {
 			ath10k_err(ar,
 				   "failed to send ext resource cfg command : %d\n",

drivers/net/wireless-4.2/ath/ath10k/core.h
@@ -706,6 +706,8 @@ struct ath10k {
 		u32 max_spatial_stream;
 		u32 cal_data_len;
 
+		enum wmi_host_platform_type platform_type;
+
 		struct ath10k_hw_params_fw {
 			const char *dir;
 			const char *fw;

commit 789b1ab9e5c1b032ebcd62cfa644f3eacce7728e

CHROMIUM: ath10k: Fix platform type for QCA40XX
Found this error while rebasing the patches, Platform type is wrongly
assigned to QCA99X0 instead of QCA40XX. This commit fixes this.

fixes: fad686618b89 ("CHROMIUM: ath10k: Configure platform
type for QCA40XX")

BUG=chrome-os-partner:49260
TEST=build and tested basic wifi functionality on gale

Change-Id: Iada3bbb694bde333374f649efa142bf25b933ed1
Signed-off-by: Anilkumar Kolli <akolli@codeaurora.org>
Signed-off-by: Maharaja Kennadyrajan <mkenna@codeaurora.org>
Reviewed-on: https://chromium-review.googlesource.com/346062
Commit-Ready: Srinivasa duvvuri <sduvvuri@chromium.org>
Tested-by: Ashok Raj Nagarajan <Arnagara@qti.qualcomm.com>
Reviewed-by: Srinivasa duvvuri <sduvvuri@chromium.org>

drivers/net/wireless-4.2/ath/ath10k/core.c
@@ -167,7 +167,6 @@ static const struct ath10k_hw_params ath10k_hw_params_list[] = {
 		.rx_chain_mask = 0xf,
 		.max_spatial_stream = 4,
 		.cal_data_len = 12064,
-		.platform_type = WMI_HOST_PLATFORM_LOW_PERF,
 		.fw = {
 			.dir = QCA99X0_HW_2_0_FW_DIR,
 			.fw = QCA99X0_HW_2_0_FW_FILE,
@@ -231,6 +230,7 @@ static const struct ath10k_hw_params ath10k_hw_params_list[] = {
 		.rx_chain_mask = 0x3,
 		.max_spatial_stream = 2,
 		.cal_data_len = 12064,
+		.platform_type = WMI_HOST_PLATFORM_LOW_PERF,
 		.fw = {
 			.dir = QCA4019_HW_1_0_FW_DIR,
 			.fw = QCA4019_HW_1_0_FW_FILE,