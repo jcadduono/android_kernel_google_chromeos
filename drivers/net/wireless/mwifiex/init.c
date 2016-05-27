14c1a4b84e0d9a8a33aa11a781f33797620c5930
Wei-Ning Huang <wnhuang@chromium.org>
2016-05-27 19:23:02 +0800

    FROMLIST: mwifiex: fix race condition when downloading firmware
    The action 'check for winner' and 'download firmware' should be an
    atomic action. This is true for btmrvl driver but not mwmfiex, which
    cause firmware download to fail when the following scenario happens:
    
    1) mwifiex check winner status: true
    2) btmrvl check winner status: true, and start downloading firmware
    3) mwifiex tries to download firmware, but failed because btmrvl is
    already downloading.
    
    This won't happen if 1) and 3) is an atomic action. This patch adds
    sdio_claim/release_host call around those two actions to make sure it's
    atomic.
    
    Signed-off-by: Wei-Ning Huang <wnhuang@chromium.org>
    (am from https://patchwork.kernel.org/patch/9138245/)
    
    BUG=chrome-os-partner:53209
    TEST=reboot oak for 500 times and no wifi interface drop
    
    Change-Id: Ie9c0ad90e63061216221428a0097a16d2c3f6929
    Reviewed-on: https://chromium-review.googlesource.com/347151
    Commit-Ready: Wei-Ning Huang <wnhuang@chromium.org>
    Tested-by: Wei-Ning Huang <wnhuang@chromium.org>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>

--- drivers/net/wireless/mwifiex/init.c
+++ drivers/net/wireless/mwifiex/init.c
@@ -24,6 +24,7 @@
 #include "main.h"
 #include "wmm.h"
 #include "11n.h"
+#include "sdio.h"
 
 /*
  * This function adds a BSS priority table to the table list.
@@ -745,8 +746,12 @@ mwifiex_shutdown_drv(struct mwifiex_adapter *adapter)
 int mwifiex_dnld_fw(struct mwifiex_adapter *adapter,
 		    struct mwifiex_fw_image *pmfw)
 {
+	struct sdio_mmc_card *card = adapter->card;
 	int ret;
 	u32 poll_num = 1;
+
+	if (adapter->iface_type == MWIFIEX_SDIO)
+		sdio_claim_host(card->func);
 
 	if (adapter->if_ops.check_fw_status) {
 		/* check if firmware is already running */
@@ -754,7 +759,7 @@ int mwifiex_dnld_fw(struct mwifiex_adapter *adapter,
 		if (!ret) {
 			mwifiex_dbg(adapter, MSG,
 				    "WLAN FW already running! Skip FW dnld\n");
-			return 0;
+			goto release_host;
 		}
 	}
 
@@ -767,7 +772,7 @@ int mwifiex_dnld_fw(struct mwifiex_adapter *adapter,
 		if (ret) {
 			mwifiex_dbg(adapter, MSG,
 				    "WLAN read winner status failed!\n");
-			return ret;
+			goto release_host;
 		}
 
 		if (!adapter->winner) {
@@ -783,7 +788,7 @@ int mwifiex_dnld_fw(struct mwifiex_adapter *adapter,
 		if (ret) {
 			mwifiex_dbg(adapter, ERROR,
 				    "prog_fw failed ret=%#x\n", ret);
-			return ret;
+			goto release_host;
 		}
 	}
 
@@ -793,6 +798,9 @@ poll_fw:
 	if (ret)
 		mwifiex_dbg(adapter, ERROR,
 			    "FW failed to be active in time\n");
+release_host:
+	if (adapter->iface_type == MWIFIEX_SDIO)
+		sdio_release_host(card->func);
 
 	return ret;
 }
