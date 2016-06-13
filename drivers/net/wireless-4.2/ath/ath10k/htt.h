f3e39ddc8670c43f6edaeab93a38b480711037b6
Maharaja Kennadyrajan <mkenna@codeaurora.org>
2016-06-13 17:49:22 +0530

    CHROMIUM: ath10k: Extended the HTT stats support to retrieve Mu-MIMO related stats
    This patch extended the bit mask value for the HTT stats
    (Mu-MIMO related) which are retrieved via tracing.
    
    BUG=None
    TEST=Mu-MIMO HTT stats are captured and verified after apply this change
    
    Change-Id: I49afb40ca2afe750bb81210a3903b65425c9e6d2
    Signed-off-by: Maharaja Kennadyrajan <mkenna@codeaurora.org>
    Reviewed-on: https://chromium-review.googlesource.com/351990
    Commit-Ready: Ashok Raj Nagarajan <Arnagara@qti.qualcomm.com>
    Tested-by: Ashok Raj Nagarajan <Arnagara@qti.qualcomm.com>
    Reviewed-by: Srinivasa duvvuri <sduvvuri@chromium.org>

--- drivers/net/wireless-4.2/ath/ath10k/htt.h
+++ drivers/net/wireless-4.2/ath/ath10k/htt.h
@@ -1807,7 +1807,7 @@ void ath10k_htt_rx_free(struct ath10k_htt *htt);
 void ath10k_htt_htc_tx_complete(struct ath10k *ar, struct sk_buff *skb);
 void ath10k_htt_t2h_msg_handler(struct ath10k *ar, struct sk_buff *skb);
 int ath10k_htt_h2t_ver_req_msg(struct ath10k_htt *htt);
-int ath10k_htt_h2t_stats_req(struct ath10k_htt *htt, u8 mask, u64 cookie);
+int ath10k_htt_h2t_stats_req(struct ath10k_htt *htt, u32 mask, u64 cookie);
 int ath10k_htt_send_frag_desc_bank_cfg(struct ath10k_htt *htt);
 int ath10k_htt_send_rx_ring_cfg_ll(struct ath10k_htt *htt);
 int ath10k_htt_h2t_aggr_cfg_msg(struct ath10k_htt *htt,
