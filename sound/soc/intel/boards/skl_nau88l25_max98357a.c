115dc684b97c44926ef9a3e8fd1ea68688d1a8ba
Chintan Patel <chintan.m.patel@intel.com>
2016-06-16 14:07:59 -0700

    Revert "CHROMIUM: HACK: ASoC: Rename wake on voice device"
    This patch reverts commit a8b3e36b230f53a9d0ce36aa8652deec8e291003.
    Stream name is set correctly based on Wake On Voice functionality.
    
    BUG=chrome-os-partner:48270
    TEST=with the whole series applied WoV works
    1. cras_test_client --listen_for_hotword
       Expect to see print from test console:
       got hotword 4096 frames
       ...
    
    Change-Id: I40ebcaa8eba676b3679e5e5644eef16fe9817d85
    Signed-off-by: Chintan Patel <chintan.m.patel@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/353469
    Commit-Ready: Ben Zhang <benzh@chromium.org>
    Tested-by: Ben Zhang <benzh@chromium.org>
    Reviewed-by: Ben Zhang <benzh@chromium.org>

--- sound/soc/intel/boards/skl_nau88l25_max98357a.c
+++ sound/soc/intel/boards/skl_nau88l25_max98357a.c
@@ -415,7 +415,7 @@ static struct snd_soc_dai_link skylake_dais[] = {
 	},
 	[SKL_DPCM_AUDIO_REF_CP] = {
 		.name = "Skl Audio Reference cap",
-		.stream_name = "Refcap",
+		.stream_name = "Wake on Voice",
 		.cpu_dai_name = "Reference Pin",
 		.codec_name = "snd-soc-dummy",
 		.codec_dai_name = "snd-soc-dummy-dai",
