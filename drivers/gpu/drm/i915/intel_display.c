2e279b9a4e5c48415f6d3c915d85173de3fcd221
Ville Syrjälä <ville.syrjala@linux.intel.com>
2016-06-17 17:13:10 +0300

    BACKPORT: drm: Deal with rotation in drm_plane_helper_check_update()
    drm_plane_helper_check_update() needs to account for the plane rotation
    for correct clipping/scaling calculations. Do so.
    
    There was an earlier attempt [1] to add this into
    intel_check_primary_plane() but I requested that it'd be put into the
    helper instead. An updated patch never materialized AFAICS, so I went
    ahead and cooked one up myself.
    
    v2: Deal with new drm_plane_helper_check_update() callers
    
    [1] https://patchwork.freedesktop.org/patch/65177/
    Cc: Nabendu Maiti <nabendu.bikash.maiti@intel.com>
    Cc: Noralf Trnnes <noralf@tronnes.org>
    Cc: CK Hu <ck.hu@mediatek.com>
    Cc: Mark Yao <mark.yao@rock-chips.com>
    Cc: Russell King <rmk+kernel@armlinux.org.uk>
    Signed-off-by: Ville Syrjl <ville.syrjala@linux.intel.com>
    Reviewed-by: Patrik Jakobsson <patrik.r.jakobsson@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1466172790-10025-1-git-send-email-ville.syrjala@linux.intel.com
    
    (cherry picked from commit 9b8b013dde18ea1ff2392ff2963680c2271efc19)
    Signed-off-by: Bibby Hsieh <bibby.hsieh@mediatek.com>
    Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>
    [djkurtz: skipping files not in chromeos-3.18:
    	drivers/gpu/drm/drm_simple_kms_helper.c
    	drivers/gpu/drm/rockchip/rockchip_drm_vop.c
    ]
    
    BUG=chrome-os-partner:54665
    TEST=boot to ui on elm
    TEST=HDMI work normal (plug/unplug/replug, display normal, show/hide
    cursor)
    TEST=backlight on/off
    TEST=suspend resume stress test finished 10000 iterations.
    TEST=chameleon display_HotPlugAtSuspend.extended_unplugged
    TEST=chameleon display_HotPlugAtSuspend.mirrored_unplugged
    
    Change-Id: I72de503ad68e564913a282dbc0164cf17d33cd61
    Reviewed-on: https://chromium-review.googlesource.com/364393
    Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Wei-Ning Huang <wnhuang@chromium.org>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -13531,6 +13531,7 @@ intel_check_primary_plane(struct drm_plane *plane,
 
 	return drm_plane_helper_check_update(plane, crtc, fb, &state->src,
 					     &state->dst, &state->clip,
+					     state->base.rotation,
 					     min_scale, max_scale,
 					     can_position, true,
 					     &state->visible);
@@ -13715,6 +13716,7 @@ intel_check_cursor_plane(struct drm_plane *plane,
 
 	ret = drm_plane_helper_check_update(plane, crtc, fb, &state->src,
 					    &state->dst, &state->clip,
+					    state->base.rotation,
 					    DRM_PLANE_HELPER_NO_SCALING,
 					    DRM_PLANE_HELPER_NO_SCALING,
 					    true, true, &state->visible);
