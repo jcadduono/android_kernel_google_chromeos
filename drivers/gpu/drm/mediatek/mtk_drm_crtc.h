c6c19917773240b19755db498531121dfebd1cfe
Daniel Kurtz <djkurtz@chromium.org>
2016-07-22 17:37:15 +0800

    FROMLIST: drm/mediatek: Remove mtk_drm_crtc_check_flush
    This function no longer exists.
    
    Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>
    (am from https://patchwork.kernel.org/patch/9262595/)
    Signed-off-by: Bibby Hsieh <bibby.hsieh@mediatek.com>
    
    BUG=chrome-os-partner:54665
    TEST=boot to ui on elm
    TEST=HDMI work normal (plug/unplug/replug, display normal, show/hide cursor)
    TEST=backlight on/off
    TEST=suspend resume stress test finished 10000 iterations.
    TEST=chameleon display_HotPlugAtSuspend.extended_unplugged
    TEST=chameleon display_HotPlugAtSuspend.mirrored_unplugged
    
    Change-Id: I91747f03fb75afdafe35d6ad9944afc39790b29f
    Reviewed-on: https://chromium-review.googlesource.com/362261
    Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Wei-Ning Huang <wnhuang@chromium.org>

--- drivers/gpu/drm/mediatek/mtk_drm_crtc.h
+++ drivers/gpu/drm/mediatek/mtk_drm_crtc.h
@@ -22,7 +22,6 @@
 
 int mtk_drm_crtc_enable_vblank(struct drm_device *drm, unsigned int pipe);
 void mtk_drm_crtc_disable_vblank(struct drm_device *drm, unsigned int pipe);
-void mtk_drm_crtc_check_flush(struct drm_crtc *crtc);
 void mtk_crtc_vblank_irq(struct drm_crtc *crtc);
 void mtk_crtc_target_line_irq(struct drm_crtc *crtc);
 int mtk_drm_crtc_create(struct drm_device *drm_dev,
