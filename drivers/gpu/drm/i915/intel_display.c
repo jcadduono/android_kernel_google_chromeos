6eaab9ea9e3db6921a569bbddc1cd9453805604b
Stéphane Marchesin <marcheu@chromium.org>
2016-10-28 15:44:28 -0700

    Revert "BACKPORT: drm/i915: Only run commit when crtc is active, v2."
    This reverts commit 34055f9fc3ffcdece8fae3efc2589b24419f02f6.
    
    BUG=chrome-os-partner:57702
    TEST=compiles
    
    Change-Id: Ie81f30f51e84fb9a9fc24dea2c58a1037dac44ca
    Reviewed-on: https://chromium-review.googlesource.com/405083
    Commit-Ready: Stéphane Marchesin <marcheu@chromium.org>
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -13204,8 +13204,7 @@ static int intel_atomic_commit(struct drm_device *dev,
 		if (!modeset)
 			intel_pre_plane_update(intel_crtc);
 
-		if (crtc->state->active)
-			drm_atomic_helper_commit_planes_on_crtc(crtc_state);
+		drm_atomic_helper_commit_planes_on_crtc(crtc_state);
 
 		if (put_domains)
 			modeset_put_power_domains(dev_priv, put_domains);
@@ -13532,6 +13531,9 @@ intel_commit_primary_plane(struct drm_plane *plane,
 
 	crtc = crtc ? crtc : plane->crtc;
 
+	if (!crtc->state->active)
+		return;
+
 	dev_priv->display.update_primary_plane(crtc, fb,
 					       state->src.x1 >> 16,
 					       state->src.y1 >> 16);
@@ -13560,7 +13562,8 @@ static void intel_begin_crtc_commit(struct drm_crtc *crtc,
 		intel_update_watermarks(crtc);
 
 	/* Perform vblank evasion around commit operation */
-	intel_pipe_update_start(intel_crtc, &intel_crtc->start_vbl_count);
+	if (crtc->state->active)
+		intel_pipe_update_start(intel_crtc, &intel_crtc->start_vbl_count);
 
 	if (modeset)
 		return;
@@ -13576,7 +13579,8 @@ static void intel_finish_crtc_commit(struct drm_crtc *crtc,
 {
 	struct intel_crtc *intel_crtc = to_intel_crtc(crtc);
 
-	intel_pipe_update_end(intel_crtc, intel_crtc->start_vbl_count);
+	if (crtc->state->active)
+		intel_pipe_update_end(intel_crtc, intel_crtc->start_vbl_count);
 }
 
 /**
@@ -13764,7 +13768,8 @@ intel_commit_cursor_plane(struct drm_plane *plane,
 	intel_crtc->cursor_bo = obj;
 
 update:
-	intel_crtc_update_cursor(crtc, state->visible);
+	if (crtc->state->active)
+		intel_crtc_update_cursor(crtc, state->visible);
 }
 
 static struct drm_plane *intel_cursor_plane_create(struct drm_device *dev,
