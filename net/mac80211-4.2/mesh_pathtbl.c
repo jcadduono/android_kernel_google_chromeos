0c37f85cfae2614d52043bc81e18ec8b0d90f2ac
Julan Hsu <julanhsu@google.com>
2016-11-08 10:12:53 -0800

    CHROMIUM: mesh: remove redundant mesh link broken logging
    When excessive retries and link breakage happen under heavy traffic,
    mesh link broken messages may flood kernel log. Only print the message
    when non-zero number of paths got deactivated.
    
    BUG=b:32668972
    TEST=manually on gale. sample output:
    [ 1412.377290] mesh-5000mhz:  MESH MPL the link to f6:f5:d8:1f:8a:25 is
    broken and 1 path deactivated
    [ 1412.562738] mesh-5000mhz: MESH MPLMU DIRECT dst f6:f5:d8:1f:8a:25
    next hop f6:f5:d8:1f:8a:25 metric from 11 to 134 ft 1
    
    Change-Id: Ifa773745751b729fab3d1cdf9ce7417fb069d59e
    Reviewed-on: https://chromium-review.googlesource.com/408779
    Commit-Ready: Julan Hsu <julanhsu@google.com>
    Tested-by: Julan Hsu <julanhsu@google.com>
    Reviewed-by: Srinivasa duvvuri <sduvvuri@chromium.org>

--- net/mac80211-4.2/mesh_pathtbl.c
+++ net/mac80211-4.2/mesh_pathtbl.c
@@ -864,7 +864,8 @@ void mesh_plink_broken(struct sta_info *sta)
 		}
 	}
 	rcu_read_unlock();
-	mpath_dbg(sta->sdata, " MESH MPL the link to %pM is broken and %d path deactivated \n",
+	if (paths_deactivated > 0)
+		mpath_dbg(sta->sdata, "MESH MPL the link to %pM is broken and %d path deactivated\n",
 			  sta->addr, paths_deactivated);
 }
 
