136c8665d118b78687ac8611a70352d9b8163a17
Sakari Ailus <sakari.ailus@linux.intel.com>
2016-12-16 03:24:23 +0200

    FROMLIST: vb2: dma-contig: Move vb2_dc_get_base_sgt() up
    Just move the function up. It'll be soon needed earlier than previously.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    
    (am from https://patchwork.kernel.org/patch/9477177/)
    
    BUG=chrome-os-partner:43703
    TEST=Compiled and tested on Elm.
    
    Change-Id: I8b4989ee29433cea72cd68da5634619657258a7d
    Signed-off-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/422720
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- drivers/media/v4l2-core/videobuf2-dma-contig.c
+++ drivers/media/v4l2-core/videobuf2-dma-contig.c
@@ -86,6 +86,28 @@ static unsigned long vb2_dc_get_contiguous_size(struct sg_table *sgt)
 	return size;
 }
 
+static struct sg_table *vb2_dc_get_base_sgt(struct vb2_dc_buf *buf)
+{
+	int ret;
+	struct sg_table *sgt;
+
+	sgt = kmalloc(sizeof(*sgt), GFP_KERNEL);
+	if (!sgt) {
+		dev_err(buf->dev, "failed to alloc sg table\n");
+		return NULL;
+	}
+
+	ret = dma_get_sgtable_attrs(buf->dev, sgt, buf->cookie, buf->dma_addr,
+		buf->size, &buf->attrs);
+	if (ret < 0) {
+		dev_err(buf->dev, "failed to get scatterlist from DMA API\n");
+		kfree(sgt);
+		return NULL;
+	}
+
+	return sgt;
+}
+
 /*********************************************/
 /*         callbacks for all buffers         */
 /*********************************************/
@@ -384,28 +406,6 @@ static struct dma_buf_ops vb2_dc_dmabuf_ops = {
 	.mmap = vb2_dc_dmabuf_ops_mmap,
 	.release = vb2_dc_dmabuf_ops_release,
 };
-
-static struct sg_table *vb2_dc_get_base_sgt(struct vb2_dc_buf *buf)
-{
-	int ret;
-	struct sg_table *sgt;
-
-	sgt = kmalloc(sizeof(*sgt), GFP_KERNEL);
-	if (!sgt) {
-		dev_err(buf->dev, "failed to alloc sg table\n");
-		return NULL;
-	}
-
-	ret = dma_get_sgtable_attrs(buf->dev, sgt, buf->cookie, buf->dma_addr,
-		buf->size, &buf->attrs);
-	if (ret < 0) {
-		dev_err(buf->dev, "failed to get scatterlist from DMA API\n");
-		kfree(sgt);
-		return NULL;
-	}
-
-	return sgt;
-}
 
 static struct dma_buf *vb2_dc_get_dmabuf(void *buf_priv, unsigned long flags)
 {
