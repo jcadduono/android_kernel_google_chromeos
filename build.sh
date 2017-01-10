da8748b440e7c73335cfc9e5d79a480b30a77fe9
James Christopher Adduono <jc@adduono.com>
2017-01-10 18:21:28 -0500

    Add Android kernel standalone build scripts
    Change-Id: Ic0bf141707b7c22ae12059a1189664517f93a1d8

---
+++ build.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+# Android kernel for Google ChromeOS devices build script by jcadduono
+
+################### BEFORE STARTING ################
+#
+# download a working toolchain and extract it somewhere and configure this
+# file to point to the toolchain's root directory.
+#
+# once you've set up the config section how you like it, you can simply run
+# ./build.sh [VARIANT]
+#
+###################### CONFIG ######################
+
+# root directory of android_kernel_google_chromeos git repo (default is this script's location)
+RDIR=$(pwd)
+
+[ "$VER" ] ||
+# version number
+VER=$(cat "$RDIR/VERSION")
+
+# directory containing cross-compile arm64 toolchain
+TOOLCHAIN=$HOME/build/toolchain/gcc-linaro-5.3.1-2016.05-x86_64_aarch64-linux-gnu
+
+CPU_THREADS=$(grep -c "processor" /proc/cpuinfo)
+# amount of cpu threads to use in kernel make process
+THREADS=$((CPU_THREADS + 1))
+
+############## SCARY NO-TOUCHY STUFF ###############
+
+ABORT() {
+	[ "$1" ] && echo "Error: $*"
+	exit 1
+}
+
+cd "$RDIR" || ABORT "Failed to enter $RDIR!"
+
+CONTINUE=false
+export ARCH=arm64
+export CROSS_COMPILE=$TOOLCHAIN/bin/aarch64-linux-gnu-
+export WIFIVERSION=
+
+[ -x "${CROSS_COMPILE}gcc" ] ||
+ABORT "Unable to find gcc cross-compiler at location: ${CROSS_COMPILE}gcc"
+
+while [ $# != 0 ]; do
+	if [ "$1" = "--continue" ] || [ "$1" = "-c" ]; then
+		CONTINUE=true
+	elif [ ! "$DEVICE" ]; then
+		DEVICE=$1
+	elif [ ! "$TARGET" ]; then
+		TARGET=$1
+	else
+		echo "Too many arguments!"
+		echo "Usage: ./build.sh [--continue] [device] [target defconfig]"
+		ABORT
+	fi
+	shift
+done
+
+[ "$TARGET" ] || TARGET=google
+[ "$DEVICE" ] || DEVICE=dragon
+
+DEFCONFIG=${TARGET}_${DEVICE}_defconfig
+
+[ -f "arch/$ARCH/configs/${DEFCONFIG}" ] ||
+ABORT "Config $DEFCONFIG not found in $ARCH configs!"
+
+export LOCALVERSION=$TARGET-$DEVICE-$VER
+
+CLEAN_BUILD() {
+	echo "Cleaning build..."
+	rm -rf build
+}
+
+SETUP_BUILD() {
+	echo "Creating kernel config for $LOCALVERSION..."
+	mkdir -p build
+	make -C "$RDIR" O=build "$DEFCONFIG" \
+		|| ABORT "Failed to set up build"
+}
+
+BUILD_KERNEL() {
+	echo "Starting build for $LOCALVERSION..."
+	while ! make -C "$RDIR" O=build -j"$THREADS"; do
+		read -rp "Build failed. Retry? " do_retry
+		case $do_retry in
+			Y|y) continue ;;
+			*) return 1 ;;
+		esac
+	done
+}
+
+INSTALL_MODULES() {
+	grep -q 'CONFIG_MODULES=y' build/.config || return 0
+	echo "Installing kernel modules to build/lib/modules..."
+	while ! make -C "$RDIR" O=build \
+			INSTALL_MOD_PATH="." \
+			INSTALL_MOD_STRIP=1 \
+			modules_install
+	do
+		read -rp "Build failed. Retry? " do_retry
+		case $do_retry in
+			Y|y) continue ;;
+			*) return 1 ;;
+		esac
+	done
+	rm build/lib/modules/*/build build/lib/modules/*/source
+}
+
+BUILD_CHROMEOS_KERNEL() {
+	echo "Building ChromeOS kernel image..."
+	KDIR="$RDIR/build/arch/$ARCH/boot"
+	chromeos/scripts/generate-its-script.sh -c lz4 -d "" \
+		"$KDIR/Image" "$KDIR/dts/tegra/"*.dtb > "$KDIR/Image.its" \
+		|| ABORT "Failed to generate ChromeOS Image.its"
+	build/scripts/dtc/dtc -I dts -O dtb -p 1024 \
+		< "$KDIR/Image.its" > "$KDIR/Image.fit" \
+		|| ABORT "Failed to build ChromeOS Image.fit"
+	echo "Final image: $KDIR/Image.fit"
+}
+
+if ! $CONTINUE; then
+	CLEAN_BUILD
+	SETUP_BUILD ||
+	ABORT "Failed to set up build!"
+fi
+
+BUILD_KERNEL &&
+INSTALL_MODULES &&
+BUILD_CHROMEOS_KERNEL &&
+echo "Finished building $LOCALVERSION!"
